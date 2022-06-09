a8d69b57964792628dc77c7d968e5ffcd3b74ebe
uniquexcel <uniquexcelf@gmail.com>
2022-06-09 12:55:54 -0700

    My commit to print the parent process Idenntification

--- ppid.c
+++ ppid.c
@@ -3,10 +3,8 @@
 
 /**
  * main - Prints Parent Process ID 
- * Anytime you run the process, a new process is created
- * This new process has the same output/value
+ * Run your program several times within the same shell. It should be the same.
  * Use the system call getppid (man 2 getppid)
- * check back on this code
  *
  * Return: Always 0.
  */
@@ -15,11 +13,11 @@ int main()
 {
     int p_id, p_pid;
 
-    p_id = getpid(); //Prints the Process Id
-    p_pid = getpid(); //Parent process Id
+    p_id = getpid(); /*process id*/
+    p_pid = getpid(); /*parent process id*/
 
     printf("Process ID: %d\n", p_id);
     printf("Parent Process ID: %d\n", p_pid);
 
-    return (0);
+    return 0;
 }
